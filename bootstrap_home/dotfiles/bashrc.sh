676a4a5bf27f30f772df61c7a5f4d4202ffd19d1
Nathan Buesgens <nathan@natb1.com>
2014-06-03 20:04:53 +0000

    adds a path variable

--- bootstrap_home/dotfiles/bashrc.sh
+++ bootstrap_home/dotfiles/bashrc.sh
@@ -3,3 +3,6 @@ if which tmux 2>&1 >/dev/null; then
     #if not inside a tmux session, and if no session is started, start a new session
     test -z "$TMUX" && (tmux attach || tmux new-session)
 fi
+
+# path
+export PATH=$PATH:/opt/AWS-ElasticBeanstalk-CLI-2.6.2/eb/linux/python2.7/
