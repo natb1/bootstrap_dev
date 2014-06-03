8a576eceafc09780e230840e4c6c0f3892e4ab81
Nathan Buesgens <nathan@natb1.com>
2014-06-03 20:37:13 +0000

    testing a chef recipe

---
+++ cookbook/recipes/vim-config.rb
@@ -0,0 +1,8 @@
+%w[ /home/ec2-user/.vim/autoload /home/ec2-user/.vim/bundle ].each do |path|
+  directory path do
+    owner "ec2-user"
+    group "ec2-user"
+    recursive true
+    action: create
+  end
+end
