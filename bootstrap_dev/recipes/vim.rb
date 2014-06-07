0470a822cc055852da4038ba68b24a0dba480c9b
Nathan Buesgens <nathan@natb1.com>
2014-06-07 20:28:39 +0000

    adds a vimrc to the chef deploy

--- bootstrap_dev/recipes/vim.rb
+++ bootstrap_dev/recipes/vim.rb
@@ -1,3 +1,9 @@
+template "/home/ec2-user/.gitconfig" do
+  source "gitconfig.erb"
+  owner "ec2-user"
+  group "ec2-user"
+end
+
 %w[ /home/ec2-user/.vim/autoload /home/ec2-user/.vim/bundle ].each do |path|
   directory path do
     owner "ec2-user"
