2e0e6be296c98bce87c84af521b1546fa6bdd240
Nathan Buesgens <nathan@natb1.com>
2014-06-08 04:18:46 +0000

    updates bd url to use ssh when key is available

--- bootstrap_dev/attributes/workspace.rb
+++ bootstrap_dev/attributes/workspace.rb
@@ -1,4 +1,9 @@
+include_attribute "bootstrap_dev::ssh"
+
+bd_ssh = "git@github.com:natb1/bootstrap_dev.git"
+bd_http = "https://github.com/natb1/bootstrap_dev.git"
+bootstrap_dev_url = node[:ssh][:id_rsa] ? bd_ssh : bd_http
+
 default[:workspace][:repositories] = {
-  "/home/ec2-user/workspace/bootstrap_dev" =>
-    "https://github.com/natb1/bootstrap_dev.git"
+  "/home/ec2-user/workspace/bootstrap_dev" => bootstrap_dev_url
 }
