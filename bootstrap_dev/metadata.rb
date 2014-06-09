86b0bfb90f73f909baf4da7a46f58f92791e7f0b
Nathan Buesgens <nathan@natb1.com>
2014-06-09 03:09:01 +0000

    adds a metadata file

---
+++ bootstrap_dev/metadata.rb
@@ -0,0 +1,7 @@
+name             'bootstrap_dev'
+maintainer       'Nathan Buesgens'
+maintainer_email 'nathan@natb1.com'
+license          'MIT'
+description      'initialize a dev environment'
+long_description IO.read(File.join(File.direname(File.dirname(__FILE__)), 'README.md'))
+version          '0.1.0'
